dc2d3d97ee4d7fc6e1816837f3b36b0e7de16782
dac098 <me@dac098.me>
2024-03-02 16:14:25 -0800

    the actual logic for the drop command
    forgot to commit the actual command file

---
+++ src/drop.rs
@@ -0,0 +1,27 @@
+use clap::Args;
+use anyhow::Context;
+
+use crate::db;
+
+#[derive(Debug, Args)]
+pub struct DropArgs {}
+
+pub fn drop_db(args: DropArgs) -> anyhow::Result<()> {
+    let db = db::Db::cwd_load()?;
+
+    log::info!("dropping db file: {}", db.path().display());
+
+    std::fs::remove_file(db.path())
+        .context("failed to remove db file")?;
+
+    let dir = db.path()
+        .parent()
+        .unwrap();
+
+    log::info!("dropping fsm directory: {}", dir.display());
+
+    std::fs::remove_dir(dir)
+        .context("failed to remove .fsm directory")?;
+
+    Ok(())
+}
